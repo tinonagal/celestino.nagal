0d9b00fbc0599124bc159c14fe9aef0196eff08f
tinonagal <tinonagal@gmail.com>
2023-10-26 19:20:07 +1300

    new java codes

--- Desktop/Phase1/Cucumber/client-app/src/test/java/steps/createmeeting.java
+++ Desktop/Phase1/Cucumber/client-app/src/test/java/steps/createmeeting.java
@@ -1,5 +1,27 @@
 package steps;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class createmeeting {
 
+	@Given("the user is in the create meeting page")
+	public void the_user_is_in_the_create_meeting_page() {
+	System.out.println("Step 1: Given");	
+	}
+	@When("The user enters meeting topic, number of participants, date, start time & end time in the form")
+	public void The_user_enters_meeting_topic_number_of_participants_date_start_time_end_time_in_the_form() {
+		System.out.println("Step 2: Enters Values");
+	}
+	@And("the user clicks on the creat meeting button")
+	public void The_user_clicks_on_the_create_meeting_button() {
+	System.out.println("Step 3: Click btton to enter values");	
+	}
+	@Then("The user can navigate to other pages")
+	public void The_user_can_navigate_to_other_pages() {
+	System.out.println("Step 4: The user chooses other options");	
+	}
+	
 }
--- Desktop/Phase1/Cucumber/client-app/src/test/resources/features/create-meeting.feature
+++ Desktop/Phase1/Cucumber/client-app/src/test/resources/features/create-meeting.feature
@@ -7,6 +7,6 @@ Feature: feature to validate the create meeting for client-app project.
   Scenario: Validate the information entered in the create meeting html page
     Given: the user is in the create meeting page
     When: The user enters meeting topic, number of participants, date, start time & end time in the form
-    And: the user tries to leave an information blank
-    Then: the create button will grey out and will not work as each information is required
+    And: the user clicks on the creat meeting button
+    Then: The user can navigate to other pages
 
